0745e8cf6236ebd286faa06d652c4b83d204cecb
Darrin Holst <darrinholst@gmail.com>
2010-03-22 12:05:02 -0500

    return other stuff

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,7 +3,7 @@ class MoviesController < ApplicationController
     @movies = Title.movies.find(:all)
 
     respond_to do |format|
-      format.json {render :json => @movies.to_json(:only => [:id, :name, :image, :released])}
+      format.json {render :json => @movies.to_json(:only => [:id, :name, :image, :released, :description, :rating, :running_time, :actors, :genre])}
     end
   end
 end
