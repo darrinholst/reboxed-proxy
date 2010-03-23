4b8961275a51f3edce83e22e1909164a30c7db94
Darrin Holst <darrinholst@gmail.com>
2010-03-23 12:31:47 -0500

    limit to 100

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,6 @@
 class MoviesController < ApplicationController
   def index
-    @movies = Title.most_recent.movies.find(:all, :limit => 200)
+    @movies = Title.most_recent.movies.find(:all, :limit => 100)
 
     respond_to do |format|
       format.json {render :json => @movies.to_json(:only => [:id, :name, :image, :released, :description, :rating, :running_time, :actors, :genre, :yahoo_rating])}
