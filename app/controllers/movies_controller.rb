499e3f3c77cf36fb6d8a77a6efcd263987440ce6
Darrin Holst <darrinholst@gmail.com>
2010-03-22 23:23:19 -0500

    add yahoo_rating

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,7 +3,7 @@ class MoviesController < ApplicationController
     @movies = Title.most_recent.movies.find(:all, :limit => 200)
 
     respond_to do |format|
-      format.json {render :json => @movies.to_json(:only => [:id, :name, :image, :released, :description, :rating, :running_time, :actors, :genre])}
+      format.json {render :json => @movies.to_json(:only => [:id, :name, :image, :released, :description, :rating, :running_time, :actors, :genre, :yahoo_rating])}
     end
   end
 end
