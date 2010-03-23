93eea9cf1d39e5c158a752fda1b38ddf10833b1c
Darrin Holst <darrinholst@gmail.com>
2010-03-22 20:50:28 -0500

    limit movies to 200 most recent

--- app/models/title.rb
+++ app/models/title.rb
@@ -1,4 +1,5 @@
 class Title < ActiveRecord::Base
   named_scope :movies, :conditions => {:product_type => 1}
+  named_scope :most_recent, :order => "released desc"
   named_scope :empty_metadata, :conditions => {:description => nil}
 end
