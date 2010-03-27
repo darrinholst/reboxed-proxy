7a9ab36dd7aebbafad240b73bda9c08c72e53f11
Darrin Holst <darrinholst@gmail.com>
2010-03-27 05:36:08 -0500

    up limit

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,13 +3,13 @@ class MoviesController < ApplicationController
     date = Chronic.parse(params[:since] || "8/5/76")
     cache_key = date.strftime("%Y%m%d")
     cached = Rails.cache.read(cache_key)
-    
-    unless(cached) 
-      movies = Title.oldest.movies.since(date).find(:all, :limit => 250) 
+
+    unless(cached)
+      movies = Title.oldest.movies.since(date).find(:all, :limit => 500)
       cached = movies.to_json(:only => [:id, :name, :image, :released, :description, :rating, :running_time, :actors, :genre, :yahoo_rating])
       Rails.cache.write(cache_key, cached)
     end
-    
+
     render :json => cached
   end
 end
