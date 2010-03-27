553a36c6572c9449a446b2f6186868019ca93fd5
Darrin Holst <darrinholst@gmail.com>
2010-03-27 04:37:48 -0500

    up limit to 250

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,19 +1,19 @@
 class MoviesController < ApplicationController
   def index
     date = Chronic.parse(params[:since] || "8/5/76")
-    @movies = Title.oldest.movies.since(date).find(:all, :limit => 100)
+    @movies = Title.oldest.movies.since(date).find(:all, :limit => 250)
 
     respond_to do |format|
       format.json {render :json => @movies.to_json(:only => [
-        :id, 
-        :name, 
-        :image, 
-        :released, 
-        :description, 
-        :rating, 
-        :running_time, 
-        :actors, 
-        :genre, 
+        :id,
+        :name,
+        :image,
+        :released,
+        :description,
+        :rating,
+        :running_time,
+        :actors,
+        :genre,
         :yahoo_rating
       ])}
     end
